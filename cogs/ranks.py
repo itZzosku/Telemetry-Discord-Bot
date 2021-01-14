295a0061f3811128a419c4c86d8f969dce93128d
Osku Väyrynen <osku27@hotmail.com>
2021-01-14 18:49:34 +0200

    Added Naikou and Zuko QQ in ranks.py.

--- cogs/ranks.py
+++ cogs/ranks.py
@@ -35,11 +35,23 @@ class Ranks(commands.Cog):
         korva_desc = r.html.xpath('//meta[@name="description"]/@content')
         # print(korva_desc)
 
+        r = session.get('https://eune.op.gg/summoner/userName=zukoqq')
+
+        korva2_desc = r.html.xpath('//meta[@name="description"]/@content')
+        # print(tuukka_desc)
+
+        r = session.get('https://eune.op.gg/summoner/userName=Naikou99')
+
+        naikou_desc = r.html.xpath('//meta[@name="description"]/@content')
+        # print(korva_desc)
+
         embedvar = discord.Embed(title="Ranks of the players.", color=0xbc0057)
         embedvar.add_field(name="\u200b", value=f'{rippe_desc}', inline=False)
         embedvar.add_field(name="\u200b", value=f'{valte_desc}', inline=False)
-        embedvar.add_field(name="\u200b", value=f'{tuukka_desc}', inline=False)
         embedvar.add_field(name="\u200b", value=f'{korva_desc}', inline=False)
+        embedvar.add_field(name="\u200b", value=f'{korva2_desc}', inline=False)
+        embedvar.add_field(name="\u200b", value=f'{tuukka_desc}', inline=False)
+        embedvar.add_field(name="\u200b", value=f'{naikou_desc}', inline=False)
         await ctx.send(embed=embedvar)
 
 
