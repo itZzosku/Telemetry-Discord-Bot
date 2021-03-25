beb30efeb074fd023c6c82e8140962cbdc332a79
Osku Väyrynen <osku27@hotmail.com>
2021-03-25 14:48:48 +0200

    Fixed the challenge.py

--- cogs/challenge.py
+++ cogs/challenge.py
@@ -1,1 +1,16 @@
-# TODO
+import discord
+import lichess.api
+from discord.ext import commands
+
+
+class Challenge(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['chal', 'haaste'])
+    async def challenge(self, ctx):
+        print("TODO")
+
+def setup(client):
+    client.add_cog(Challenge(client))
