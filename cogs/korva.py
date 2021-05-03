30daeba9e25fdf2c59d4a09e3a2d6d61ddd5925c
jpputkos <juuso.p.putkonen@student.jyu.fi>
2021-05-03 14:04:16 +0300

    added one account

--- cogs/korva.py
+++ cogs/korva.py
@@ -9,7 +9,7 @@ class Korva(commands.Cog):
 
     @commands.command()
     async def korva(self, ctx):
-        await ctx.send('https://eune.op.gg/multi_old/query=velivalte%2Chibana%2Coots%C3%B6pis%2Cs%C3%B6pis%2Ckorvameister%2Cidontcarelol%2Ckorva%2Calejandroabuser%2Cadmiredgame%2Crod0')
+        await ctx.send('https://eune.op.gg/multi_old/query=velivalte%2Cidontcarelol%2Chibana%2Coots%C3%B6pis%2Cs%C3%B6pis%2Calejandroabuser%2Ckorvameister%2Charleyfx%2Ckorva%2Cadmiredgame%2Crod0')
 
 
 def setup(client):
