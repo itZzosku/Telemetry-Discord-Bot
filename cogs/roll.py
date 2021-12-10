5901c2915ea4e0ff78ccf9d41c308817e14e307c
Osku Väyrynen <osku27@hotmail.com>
2021-12-10 16:10:53 +0200

    Fixed random.py

--- cogs/roll.py
+++ cogs/roll.py
@@ -8,7 +8,7 @@ class Roll(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['random', 'roll'])
+    @commands.command(aliases=['random'])
     async def roll(self, ctx):
         n = random.randint(1, 999)
         await ctx.send(n)
