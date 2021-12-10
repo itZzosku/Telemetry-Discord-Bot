a76873982d848156ba49900933c26a27304a4119
Osku Väyrynen <osku27@hotmail.com>
2021-12-10 16:06:25 +0200

    added random.py

---
+++ cogs/roll.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Roll(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['random', 'roll'])
+    async def roll(self, ctx):
+        n = random.randint(1, 999)
+        await ctx.send(n)
+
+
+def setup(client):
+    client.add_cog(Roll(client))
